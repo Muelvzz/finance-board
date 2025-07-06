12a76e22c42d94c90512e7fd916f79cdb86c95e1
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-06 12:00:35 +0800

    added 'delete' feature

--- main.py
+++ main.py
@@ -31,7 +31,21 @@ class FinanceDashboard():
             st.plotly_chart(figure)
 
             return figure
-    
+        
+    def delete_amount(self, get_id):
+        with sqlite3.connect("database.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM finance WHERE id = ?", (get_id,))
+            conn.commit()
+
+    def view(self):
+        # col1, col2, col3, col4, col5 = st.columns(5)
+
+        with sqlite3.connect("database.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM finance")
+            result = cursor.fetchall()
+            return result
 
 class Frontend():
 
@@ -39,7 +53,8 @@ class Frontend():
         self.current_time = datetime.now().strftime("%m-%d %H:%M")
         
     def run_web(self):
-        st.session_state.dashboard = FinanceDashboard()
+        if "dashboard" not in st.session_state:
+            st.session_state.dashboard = FinanceDashboard()
 
         st.markdown("<div style='text-align: center;'>"
                     "<h1>Finance Dashboard</h1>"
@@ -53,15 +68,41 @@ class Frontend():
             st.success("Successfully Added")
 
     def show_graph(self):
-        st.session_state.dashboard = FinanceDashboard()
+        if "dashboard" not in st.session_state:
+            st.session_state.dashboard = FinanceDashboard()
         graph_clicked = st.button("Show Budget Graph")
 
         if graph_clicked:
             st.session_state.dashboard.graph()
 
+    def view_amount(self):
+        if "dashboard" not in st.session_state:
+            st.session_state.dashboard = FinanceDashboard()
+
+        if "view_mode" not in st.session_state:
+            st.session_state.view_mode = False
+
+        view_clicked = st.button("View Budget", key="view")
+
+        if view_clicked:
+            st.session_state.view_mode = True
+
+        if st.session_state.view_mode:
+            amount_entries = st.session_state.dashboard.view()
+            if amount_entries:
+                for row in amount_entries:
+                    id1 = row[0]
+                    st.write(row[1])
+                    st.write(row[2])
+                    delete_clicked = st.button("Delete", key=f"delete_{row[0]}")
+                    if delete_clicked:
+                        st.session_state.dashboard.delete_amount(id1)
+
+
 if __name__ == "__main__":
     frontend = Frontend()
-    
+
     frontend.run_web()
     frontend.show_graph()
+    frontend.view_amount()
 
